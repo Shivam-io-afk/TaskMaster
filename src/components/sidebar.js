13563ac0ac7d477b88ea427d9886095798a99dcf
Shivam <sm2552744@gmail.com>
2025-01-14 22:20:53 +0530

    taskmaster

---
+++ src/components/sidebar.js
@@ -0,0 +1,12 @@
+import '../styles/main.css'
+
+function SideBar(){
+    return (
+        <div className="mainContainer">
+                
+        </div>
+    )
+}
+
+export default SideBar;
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import SideBar from './components/sidebar';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div  style={{position:"relative",height:"100vh",width:"100vw",border:"1px solid red",overflow:"hidden"}}>
+      <SideBar />
+      <App />
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
