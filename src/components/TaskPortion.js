7d274ab2188123ce9618bad47e8f3b68457cfc88
Shivam <sm2552744@gmail.com>
2025-02-14 01:35:22 +0530

    taskmaster

--- src/components/TaskPortion.js
+++ src/components/TaskPortion.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import '../App.css';
+import { FaRegCircleRight } from 'react-icons/fa6'; 
+import { FaStar } from 'react-icons/fa6';
 
 const tasks = () => {
     return (
         <div className="taskcontainer">
             <div className="main_tskcnt">
                 <div className="cnt w_fr">
-                    <div className="indicator"></div>
-                    <div className="slider_bx"></div>
+                    <div className="indicator">Todo</div>
+                    <div className="slider_bx">
+
+                        
+                        <div className='T_blocks bg_1'>
+                            <div>
+                                <span><FaStar style={{color:"orange"}}/> Today</span>
+                                <FaRegCircleRight className='icns'/>
+                            </div>
+                            <span className='titlee'>Create A visual Style Guide</span>
+                            <div>
+                                <span>9:00 - 9:30</span>
+                                <div>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                        <div className='T_blocks bg_2'></div>
+                        <div className='T_blocks bg_1'></div>
+                    </div>
                 </div>
-                <div className="cnt w_sc"></div>
-                <div className="cnt w_thr"></div>
-                <div className="cnt w_frt"></div>
+                <div className="cnt w_sc">
+                    <div className="indicator">In Progress</div>
+                    <div className="slider_bx">
+                        <div className='T_blocks bg_3'></div>
+                        <div className='T_blocks bg_1'></div>
+                        <div className='T_blocks bg_3'></div>
+                    </div>
+                </div>
+                <div className="cnt w_thr">
+                    <div className="indicator">Completed</div>
+                    <div className="slider_bx">
+                        <div className='T_blocks bg_4'></div>
+                        <div className='T_blocks bg_4'></div>
+                        <div className='T_blocks bg_4'></div>
+                    </div>
+                </div>
+                <div className="cnt w_frt">
+                    <div className="indicator">Overdue</div>
+                    <div className="slider_bx">
+                        <div className='T_blocks bg_4'></div>
+                        <div className='T_blocks bg_4'></div>
+                        <div className='T_blocks bg_4'></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
